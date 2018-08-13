51c1984c68e27f5e550663ef1f89df69e7eee006
DawidGaleziewski <dawidgalsino@vp.pl>
2018-08-13 18:48:56 +0000

    basic blog functionality

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,52 @@
+class ArticlesController < ApplicationController
+  before_action :find_article, only: [:show, :update, :destroy, :edit]
+  
+  def index 
+    @articles= Article.all
+  end 
+  
+  def new 
+   @article =  Article.new 
+  end
+  
+  def create 
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:notice] = 'all fine'
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end  
+  end
+  
+  def show
+  
+  end
+  
+  def edit 
+    
+  end
+  
+  def update
+    @article.update(article_params)
+    redirect_to article_path(@article)
+  end
+  
+  def destroy
+    @article.destroy
+    redirect_to articles_path
+  end
+  
+  private
+  
+  def article_params
+    params.require(:article).permit(:title, :description, :author)
+  end
+  
+  def find_article
+    @article = Article.find(params[:id])
+    
+  end
+  
+  
+end
